3c6a514d8ac24feaa1f436a0993fa4bbe29696b5
jenjose72 <jenjose72@gmail.com>
2026-02-26 14:46:15 +0530

    initial

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+
+    <h1>Jen Jose Jeeson - 2023BCS0017</h1>
+    
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
